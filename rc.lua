0a66564ee605c0f59f8ff82ca792527891bb95d8
Rainux Luo <rainux@gmail.com>
2010-09-30 00:24:09 +0800

    Use eth0

--- rc.lua
+++ rc.lua
@@ -92,7 +92,7 @@ separator.text = ' || '
 
 -- Network usage widget
 netwidget = widget({ type = 'textbox' })
-vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">${eth1 down_kb}</span> <span color="#7F9F7F">${eth1 up_kb}</span>', 3)
+vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">${eth0 down_kb}</span> <span color="#7F9F7F">${eth0 up_kb}</span>', 3)
 
 -- CPU usage widget (textbox)
 cpuwidget = widget({ type = 'textbox' })
